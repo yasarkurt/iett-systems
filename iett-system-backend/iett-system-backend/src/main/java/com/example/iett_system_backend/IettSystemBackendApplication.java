9df59bd3d1af612962f666f79f6c7041aa416a8a
yasarkurt <kurt@teknopar.com.tr>
2025-03-01 11:03:28 +0300

    proje yapısı oluşturuldu

---
+++ iett-system-backend/iett-system-backend/src/main/java/com/example/iett_system_backend/IettSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.iett_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IettSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IettSystemBackendApplication.class, args);
+	}
+
+}
---
+++ iett-system-backend/iett-system-backend/src/test/java/com/example/iett_system_backend/IettSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iett_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IettSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
